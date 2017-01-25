20b30cb0878a9142ae5094b5d96ee11d338f5ccf
Salomé Russier <salome.russier@gmail.com>
2017-01-25 12:19:04 +0100

    Toast class

---
+++ TestFramework/TestFramework/Toast.swift
@@ -0,0 +1,15 @@
+//
+//  Toast.swift
+//  TestFramework
+//
+//  Created by Salomé Russier on 25/01/2017.
+//  Copyright © 2017 Salomé Russier. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class Toast : UIView {
+    
+    
+}
